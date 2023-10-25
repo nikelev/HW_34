dc1d43ba66674de8ed25b2b5517d2ceb0b3ed216
nikelev <nikelev777@gmail.com>
2023-10-25 18:21:32 +0200

    commit3

--- src/ait/arrays/utils/ArrayTools.java
+++ src/ait/arrays/utils/ArrayTools.java
@@ -25,54 +25,57 @@ public class ArrayTools {
             if (pedicate.test(arr[i])) {
                 return arr[i];
             }
-        }return null;
+        }
+        return null;
     }
-    public static <T extends Comparable <T  >> void bableSort (T[]arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
-                if (arr[j].compareTo(arr[j+1])>0){
-                    T t=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=t;
+
+    public static <T extends Comparable<T>> void bableSort(T[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    T t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
                 }
             }
         }
     }
 
-    public static <T > void bableSort (T[]arr, Comparator<T> comparator){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
-                if (comparator.compare(arr[j],arr[j+1])>0){
-                    T t=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=t;
+    public static <T> void bableSort(T[] arr, Comparator<T> comparator) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (comparator.compare(arr[j], arr[j + 1]) > 0) {
+                    T t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
                 }
             }
         }
     }
-    public static  <T >int binarySearch(T[] arr, T value,Comparator<T> comparator) {
+
+    public static <T> int binarySearch(T[] arr, T value, Comparator<T> comparator) {
         int l = 0;
         int r = arr.length - 1;
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (arr[mid] == value) {
+            if (comparator.compare(arr[mid], value) == 0) {
                 return mid;
             }
-            if (comparator.compare(value,arr[mid])  >0 ) {
+            if (comparator.compare(value, arr[mid]) > 0) {
                 l = mid + 1;
             } else {
                 r = mid - 1;
             }
         }
-        return -l-1;
+        return -l - 1;
     }
 
-    public static <T extends Comparable <T  >>int binarySearch(T[] arr, T value) {
+    public static <T extends Comparable<T>> int binarySearch(T[] arr, T value) {
         int l = 0;
         int r = arr.length - 1;
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (arr[mid].compareTo(value)==0 ) {
+            if (arr[mid].compareTo(value) == 0) {
                 return mid;
             }
             if (value.compareTo(arr[mid]) > 0) {
@@ -81,10 +84,8 @@ public class ArrayTools {
                 r = mid - 1;
             }
         }
-        return -l-1;
+        return -l - 1;
     }
-
-
 
 
 }
